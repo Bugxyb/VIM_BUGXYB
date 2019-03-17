7147ab173419f357e3ba8078096db676439a130a
Xunyunbo <xunyunbo@do-global.com>
2019-03-18 01:24:52 +0800

    update

---
+++ tools.sh
@@ -0,0 +1,33 @@
+#!/usr/bash
+
+function mac_brew() {
+    ls -al
+    brew install npm composer fzf the_silver_searcher
+}
+
+
+function bash_server(){
+    npm i -g bash-language-server
+}
+
+function python_server() {
+    pip install python-language-server[all]
+    pip install -U setuptools
+}
+
+
+function main() {
+    case $1 in
+        'mac')
+            mac_brew
+        ;;
+        'bash')
+            bash_server
+        ;;
+        'python')
+            python_server
+        ;;
+    esac
+
+}
+main $@
